1d3c8bd529a981cf9bea58bc13e478a2fdd30a22
EsdrasC24 <josuequeraleswd2420@gmail.com>
2025-02-11 13:18:52 -0400

    fix: add base template files

--- anuncios/models.py
+++ anuncios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    edicion = models.CharField(max_length=100)
+    estado = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    condiciones_intercambio = models.TextField()
+    fecha_publicacion = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
